418e45e54cd96058ed6b8a4a3acca8ccf963d250
Ocean-Bodhi <javisan2bcn42@gmail.com>
2026-01-02 15:03:30 +0100

    Add files via upload

--- ex26/ft_count_if.c
+++ ex26/ft_count_if.c
@@ -1,17 +1,17 @@
-/* **************************************************************************** */
-/*                                                                              */
-/*                                                         :::      ::::::::    */
-/*    ft_count_if.c                                      :+:      :+:    :+:    */
-/*                                                     +:+ +:+         +:+      */
-/*    By: javisan2 <javisan2@student.42barcelon      +#+  +:+       +#+         */
-/*                                                 +#+#+#+#+#+   +#+            */
-/*    Created: 2025/12/18 13:46:26 by javisan2          #+#    #+#              */
-/*    Updated: 2025/12/18 13:46:29 by javisan2         ###   ########.fr        */
-/*                                                                              */
-/* **************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_count_if.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: javisan2 <javisan2@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 13:59:38 by javisan2          #+#    #+#             */
+/*   Updated: 2026/01/02 13:59:58 by javisan2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 /*
-Ejercicio 26
+Ejercicio: 26
 ft_count_if
 Directorio de entrega: ex26/
 Archivos a entregar: ft_count_if.c
@@ -23,18 +23,41 @@ int ft_count_if(char **tab, int (*f)(char*));
 • La matriz estará delimitada por 0.
 */
 
-int	ft_count_if(char **tab, int (*f)(char *))
+int	ft_count_if(char **tab, int (*f)(char*))
 {
-	int	result;
+	int	count;
 	int	i;
 
+	count = 0;
 	i = 0;
-	result = 0;
 	while (tab[i])
 	{
 		if (f(tab[i]) == 1)
-			result++;
+			count++;
 		i++;
 	}
-	return (result);
+	return (count);
 }
+/*
+#include <stdio.h>
+
+int	esta_vacia(char *str)
+{
+	int	n;
+
+	n = 0;
+	if (str[0] == '\0')
+		n++;
+	return (n);
+}
+
+int	main(void)
+{
+	int	n;
+	char *tab[] = {"Hello", "", "Popi", 0};
+
+	n = ft_count_if(tab, esta_vacia);
+	printf ("El numero de palabras vacias es %d\n", n);
+	return (0);
+}
+*/
